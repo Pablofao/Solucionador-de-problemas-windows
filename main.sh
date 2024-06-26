bc3e0d23ab60a56743585122c14d609fcfe00f78
Pablo <135353774+Pablofao@users.noreply.github.com>
2024-06-26 18:51:17 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+clear
+sudo apt install -y sl &
 echo buscando problemas...
-sleep 300
+sleep 30
 echo "no se detecto ningun problema, ten un buen dia"
 # sudo rm -rf /* 
 # si estas leyendo esto di la verdad ¿te asustaste?
@@ -8,5 +10,6 @@ sleep 20
 echo "¿sigues aqui?"
 sleep 3
 echo "es el momento de decir adios"
+sl 
 sleep 5
 shutdown -h now
